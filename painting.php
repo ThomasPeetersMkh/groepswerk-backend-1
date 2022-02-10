e64550e91b60860d6bc809282e787b817811f813
Jaric De Becker <jaric.debecker@gmail.com>
2022-02-10 19:00:44 +0100

    detailpagina painting aangepast

--- painting.php
+++ painting.php
@@ -8,7 +8,15 @@ PrintHeader();
 
 if ( ! is_numeric( $_GET["kunst_id"] ) ) die("Foutieve GET parameter!");
 
-$rows = GetData( "select * from kunst where kunst_id=" . $_GET["kunst_id"] );
+$rows = GetData( "select kunst_naam, kunst_omschrijving from Kunst where kunst_id=" . $_GET["kunst_id"] );
+
+/*
+$rows = GetData( "select kunst_naam, kunst_omschrijving, afb_path, artiest_voornaam, artiest_achternaam, stijl_naam
+from Kunst inner join Afbeelding on Kunst.kunst_id = Afbeelding.kunst_id
+inner join Artiest_Kunst on Kunst.kunst_id = Artiest_Kunst.kunst_id
+inner join Artiest on Artiest_Kunst.artiest_id = Artiest.artiest_id
+inner join Stijl on Kunst.stijl_id = Stijl.stijl_id where kunst_id=" . $_GET["kunst_id"] );
+ */
 
 foreach ($rows as $row){
     print '<div class="showcase">';
